2d908b42e99fae4e6c58deb975cc313388e38c0d
NycuRO <nghergu@yahoo.com>
2016-10-07 20:28:35 +0300

    Update MainClass.java

--- src/main/java/SynapseFixInventory/MainClass.java
+++ src/main/java/SynapseFixInventory/MainClass.java
@@ -5,7 +5,7 @@ import cn.nukkit.event.Listener;
 import cn.nukkit.event.player.PlayerJoinEvent;
 import cn.nukkit.plugin.PluginBase;
 
-public class SynapseFixInventory extends PluginBase implements Listener {
+public class MainClass extends PluginBase implements Listener {
 
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
