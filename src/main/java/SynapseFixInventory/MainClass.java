5cd0bca584ddbbde59ee2d975b74a0481aa354cc
NycuRO <nghergu@yahoo.com>
2016-10-07 20:08:37 +0300

    Create MainClass.java

---
+++ src/main/java/SynapseFixInventory/MainClass.java
@@ -0,0 +1,46 @@
+package ExamplePlugin;
+
+import cn.nukkit.command.Command;
+import cn.nukkit.command.CommandSender;
+import cn.nukkit.plugin.PluginBase;
+import cn.nukkit.utils.Config;
+import cn.nukkit.utils.TextFormat;
+import cn.nukkit.utils.Utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedHashMap;
+
+/**
+ * author: MagicDroidX
+ * NukkitExamplePlugin Project
+ */
+public class MainClass extends PluginBase {
+
+    @Override
+    public void onLoad() {
+        this.getLogger().info(TextFormat.WHITE + "I've been loaded!");
+    }
+
+    @Override
+    public void onEnable() {
+        this.getLogger().info(TextFormat.DARK_GREEN + "I've been enabled!");
+
+    @Override
+    public void onDisable() {
+        this.getLogger().info(TextFormat.DARK_RED + "I've been disabled!");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        switch (command.getName()) {
+            case "fix":
+                player.setGameMode(1);
+                player.setGameMode(0);
+                }
+                break;
+        }
+        return true;
+    }
+
+}
