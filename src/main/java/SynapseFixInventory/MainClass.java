231b42daea6e7257ba976dd4b119ac4bf694d705
NycuRO <nghergu@yahoo.com>
2016-10-20 16:31:33 +0300

    Update MainClass.java

--- src/main/java/SynapseFixInventory/MainClass.java
+++ src/main/java/SynapseFixInventory/MainClass.java
@@ -1,9 +1,17 @@
 package SynapseFixInventory;
 
+import java.util.Map;
+
+import cn.nukkit.Player;
 import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.Listener;
 import cn.nukkit.event.player.PlayerJoinEvent;
+import cn.nukkit.item.Item;
 import cn.nukkit.plugin.PluginBase;
+
+/*
+ Credits : NycuRO & haniokasai . Thanks for help.
+ */
 
 public class MainClass extends PluginBase implements Listener {
 
@@ -17,7 +25,26 @@ public class MainClass extends PluginBase implements Listener {
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent e) {
-        e.getPlayer().setGamemode(1);
-        e.getPlayer().setGamemode(0);
+    	Player player = e.getPlayer();
+    	if(player.getGamemode() == 1){
+    		player.setGamemode(0);
+    		player.setGamemode(1);
+    	}else{
+    		Map<Integer, Item> inv = player.getInventory().getContents();
+    		Item[] arm= player.getInventory().getArmorContents();
+    		if(player.getGamemode() == 2){
+    			player.setGamemode(1);
+    			player.setGamemode(2);
+    		}else if(player.getGamemode() == 0){
+    			player.setGamemode(1);
+    			player.setGamemode(0);
+    		}
+    		player.getInventory().setContents(inv);
+    		player.getInventory().setArmorContents(arm);
+    		player.getInventory().sendArmorContents(player);
+    		player.getInventory().sendContents(player);
+    	}
+
+
     }
 }
